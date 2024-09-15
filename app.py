7871b0753d7a1366d64a4b1151665bde4b92fb53
Uvesh99 <sayaidisrar@gmail.com>
2024-09-15 17:28:39 +0530

    H

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import numpy as np
-from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
 from tensorflow.keras.models import load_model
